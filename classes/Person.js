25eda36e4d700089099c02c847594e98371e9b42
kaikaine <kairijb@protonmail.com>
2023-11-09 15:11:06 -0600

    added person and bag tests and classes

--- classes/Person.js
+++ classes/Person.js
@@ -1,7 +1,20 @@
+const Bag = require("./Bag");
+
 class Person {
+  #bags;
+  constructor(name, destination) {
+    this.name = name;
+    this.destination = destination;
+    this.#bags = [];
+  }
 
+  addBags(bag) {
+    this.#bags.push(bag);
+  }
 
+  getBags() {
+    return this.#bags;
+  }
 }
-
 
 module.exports = Person;
